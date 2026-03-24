30330dbc57ed0fc95fff493ba73f0c2e1aa3ad36
Tharanga Wijethilake <tharanga.wijethilake@op.fi>
2026-03-24 23:41:26 +0200

    added for loops as started event handling

---
+++ ReactTutorial_3/test-project/src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Hello, { LatestHellow, NewHello } from './Hello'
+
+//function App() {
+
+//  return (
+//    <>
+//      <div className="App">
+
+        //<Hello name="Tharanga" enthusiasmLevel={3}/>
+        //<NewHello name="Anuja" enthusiasmLevel={163} />
+      
+//    </>
+//  )
+//}
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Hello name="Tharanga" enthusiasmLevel={3}/>
+      <NewHello name="Evis"
+      enthusiasmLevel={2}
+      >
+
+      </NewHello>
+
+      <LatestHellow name="Anuja" enthusiasmLevel={3} />
+    </div>
+  );
+};
+export default App
---
+++ ReactTutorial_3/test-project/src/main.tsx
@@ -0,0 +1,58 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import App from './App';
+import { Arrays } from './Arrays';
+
+interface Account {
+  total: number;
+}
+
+//pure function component
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+//impure function component
+function withdraw(account: Account, amount: number): void {
+  account.total -= amount;
+}
+
+function Greeter(props: { first: string }) {
+  //Doing the following is not possible in React
+  //This is a REACT feature where passed props are READ-ONLY. This is to ensure that components are pure and do not have side effects.
+  //props.first = "Tharanga";
+  return <h1>Hello, {props.first}</h1>;
+}
+function button() {
+  return <button onClick={handleClick}> Click me</button>;
+}
+
+function handleClick() {
+  alert("Button clicked!");
+}
+
+ Greeter.propTypes = {
+  first: PropTypes.string.isRequired,
+ };
+
+class NewGreeter extends React.Component<{ first: string; lastName: string }> {
+
+  render() {
+    sum(1, 2);
+    withdraw({ total: 100 }, 10);
+    return <h1>Hello, {this.props.first} {this.props.lastName}</h1>;
+  }
+}
+
+createRoot(document.getElementById('root')!).render(
+  <>
+  <button onClick={handleClick}> Click me</button>
+  <Greeter first="Tharanga" />
+  <NewGreeter first="Anuja" lastName="Wijethilake"/>
+  <App />
+  <Arrays />
+  </>,
+
+)
